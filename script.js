1279431dc0cb2260159f606d5c4463eb1621868c
Manzovince <manzovince@outlook.fr>
2020-12-10 23:36:41 +0100

    new

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function colorMode(color) {
     if (color == "dark") {
         document.getElementById('color-mode').setAttribute( "onClick", "javascript: colorMode('light');");
         document.getElementById('color-mode').innerHTML = `<svg id="sun" viewBox="0 0 24 24" width="20" height="20" stroke="#F1ECE2" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
-        document.getElementById('color-mode').style.backgroundColor = "#f1ece210";
+        document.getElementById('color-mode').style.backgroundColor = "#f1ece225";
         document.getElementById('logo').setAttribute('src', './assets/vm-logo.svg')
         document.getElementById('page').className = 'body-dark';
         document.getElementById('navigation').className = 'nav-dark';
@@ -25,7 +25,7 @@ function colorMode(color) {
     else if (color == "light") {
         document.getElementById('color-mode').setAttribute( "onClick", "javascript: colorMode('dark');" );
         document.getElementById('color-mode').innerHTML = `<svg id="moon" viewBox="0 0 24 24" width="20" height="20" stroke="#15181A" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
-        document.getElementById('color-mode').style.backgroundColor = "#15181a10";
+        document.getElementById('color-mode').style.backgroundColor = "#15181a25";
         document.getElementById('logo').setAttribute('src', './assets/vm-logo-black.svg')
         document.getElementById('page').className = 'body-light';
         document.getElementById('navigation').className = 'nav-light';
