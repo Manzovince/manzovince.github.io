9013279fea7121ae2ff8ae317f6a5c5301f0f7a8
Manzovince <manzovince@outlook.fr>
2020-12-10 19:28:48 +0100

    new cards test

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function colorMode(color) {
     else if (color == "light") {
         document.getElementById('color-mode').setAttribute( "onClick", "javascript: colorMode('dark');" );
         document.getElementById('color-mode').innerHTML = `<svg id="moon" viewBox="0 0 24 24" width="20" height="20" stroke="#15181A" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
-        document.getElementById('color-mode').style.backgroundColor = "#15181a25";
+        document.getElementById('color-mode').style.backgroundColor = "#15181a10";
         document.getElementById('logo').setAttribute('src', './assets/vm-logo-black.svg')
         document.getElementById('page').className = 'body-light';
         document.getElementById('navigation').className = 'nav-light';
