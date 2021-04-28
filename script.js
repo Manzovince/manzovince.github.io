dcd45b632bd098976e2daf6d8b26331bf199d812
Manzovince <manzovince@outlook.fr>
2021-04-28 10:49:21 +0200

    navbar hider added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Get color used by user
 /*
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
@@ -20,7 +19,7 @@ function greeting() {
 function colorMode(color) {
     if (color == "dark") {
         if (document.getElementById('color-mode')) {
-            document.getElementById('color-mode').setAttribute( "onClick", "javascript: colorMode('light');");
+            document.getElementById('color-mode').setAttribute("onClick", "javascript: colorMode('light');");
             document.getElementById('color-mode').innerHTML = `<svg id="sun" viewBox="0 0 24 24" width="20" height="20" stroke="#F1ECE2" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
             document.getElementById('color-mode').style.backgroundColor = "#f1ece225";
         }
@@ -40,10 +39,9 @@ function colorMode(color) {
             document.getElementById('project').className = 'dark';
             document.getElementsByTagName('footer')[0].className = 'dark';
         }
-    }
-    else if (color == "light") {
+    } else if (color == "light") {
         if (document.getElementById('color-mode')) {
-            document.getElementById('color-mode').setAttribute( "onClick", "javascript: colorMode('dark');" );
+            document.getElementById('color-mode').setAttribute("onClick", "javascript: colorMode('dark');");
             document.getElementById('color-mode').innerHTML = `<svg id="moon" viewBox="0 0 24 24" width="20" height="20" stroke="#15181A" stroke-width="1" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
             document.getElementById('color-mode').style.backgroundColor = "#15181a25";
         }
@@ -66,6 +64,9 @@ function colorMode(color) {
     }
 }
 
+// Navigation hider
+var prevScrollpos = window.pageYOffset;
+
 // Navigation follower
 function checkVisible(id) {
     var el = document.getElementById(id);
@@ -74,11 +75,23 @@ function checkVisible(id) {
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
-window.onscroll = function() {
+// Navigation effects
+window.onscroll = function () {
     document.getElementById('work-btn').style.borderBottom = checkVisible('work') ? '2px solid' : '';
     document.getElementById('work-btn').style.fontWeight = checkVisible('work') ? '600' : '';
     document.getElementById('about-btn').style.borderBottom = checkVisible('about') ? '2px solid' : '';
     document.getElementById('about-btn').style.fontWeight = checkVisible('about') ? '600' : '';
+
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById('navigation').style.top = "0";
+    } else {
+        document.getElementById('navigation').style.top = "-110px";
+    }
+    if (currentScrollPos <= 0) {
+        document.getElementById('navigation').style.top = "0";
+    }
+    prevScrollpos = currentScrollPos;
 };
 
 // Toggle
@@ -101,8 +114,8 @@ function randomMusic() {
 let cursor = 0;
 const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 document.addEventListener('keydown', (e) => {
-  cursor = (e.keyCode == KONAMI_CODE[cursor]) ? cursor + 1 : 0;
-  if (cursor == KONAMI_CODE.length) activate();
+    cursor = (e.keyCode == KONAMI_CODE[cursor]) ? cursor + 1 : 0;
+    if (cursor == KONAMI_CODE.length) activate();
 });
 
 // Game
